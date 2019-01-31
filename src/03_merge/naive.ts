4365ef50cf0a4764c830065d5c9cc682ce963464
Alexander KIRILOV <alexander.kirilov@devoteam.com>
2019-01-31 17:05:07 +0100

    Fixed 03 naive example

--- src/03_merge/naive.ts
+++ src/03_merge/naive.ts
@@ -7,11 +7,17 @@ const nums = of(1, 2, 3, 4, 5);
 const nums2 = of(7).pipe(delay(1000));
 const nums3 = of(9).pipe(delay(2000));
 
-const squareOddVals = pipe(
+nums.pipe(
   filter((n: number) => n % 2 !== 0),
   map(n => n * n)
-);
+).subscribe(x => console.log(x));
 
-squareOddVals(nums3).subscribe(x => console.log(x));
-squareOddVals(nums2).subscribe(x => console.log(x));
-squareOddVals(nums).subscribe(x => console.log(x));
+nums2.pipe(
+  filter((n: number) => n % 2 !== 0),
+  map(n => n * n)
+).subscribe(x => console.log(x));
+
+nums3.pipe(
+  filter((n: number) => n % 2 !== 0),
+  map(n => n * n)
+).subscribe(x => console.log(x));
